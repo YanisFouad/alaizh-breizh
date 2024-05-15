621e427cf19a7f0c963846fdff5d453cf9ba5eb6
Justine <justine.verger@etudiant.univ-rennes1.fr>
2024-05-15 11:22:02 +0200

    merge

--- html/views/layout/footer.php
+++ html/views/layout/footer.php
@@ -1,6 +1,10 @@
     </main>
     <footer>
-        &copy; BLABLABLA
+        <div>&copy; Copyright - ALHaiZ Breizh - 2024</div>
+        <nav>
+            <div><a href="">Conditions générales d'utilisation <img src="" alt=""></a></div>
+            <div><a href="">Conditions générales de vente <img src="" alt=""></a></div>
+        </nav>
     </footer>
 </body>
 </html>
--- html/views/layout/header.php
+++ html/views/layout/header.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ma page</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <!-- Problème changement nom de page ? -->
+    <title>Page d'accueil</title>
 </head>
 <body>
     <header>
-        BLABLABLA
+        <nav>
+            <ul>
+                <li><a href=""><img src="../../logo_alhaiz_breizh_fullsizeh.svg" alt=""></a></li>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Logements</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="">Inscription</a></li>
+                <!-- Deux boutons distincts ! -->
+            </ul>
+        </nav>
     </header>
     <main>
