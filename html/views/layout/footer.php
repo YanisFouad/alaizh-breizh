4e709dddf329b50825002893895a920ce8f944a6
Justine <justine.verger@etudiant.univ-rennes1.fr>
2024-05-16 08:42:49 +0200

    avancement front page accueil

--- html/views/layout/footer.php
+++ html/views/layout/footer.php
@@ -2,8 +2,8 @@
     <footer>
         <div>&copy; Copyright - ALHaiZ Breizh - 2024</div>
         <nav>
-            <div><a href="">Conditions générales d'utilisation <img src="" alt=""></a></div>
-            <div><a href="">Conditions générales de vente <img src="" alt=""></a></div>
+            <div><a href="">Conditions générales d'utilisation <span class="mdi mdi-open-in-new"></span></a></div>
+            <div><a href="">Conditions générales de vente <span class="mdi mdi-open-in-new"></span></a></div>
         </nav>
     </footer>
 </body>
--- html/views/layout/header.php
+++ html/views/layout/header.php
@@ -4,20 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/main.css">
-    <!-- Problème changement nom de page ? -->
+    <link rel="stylesheet" href="html/assets/css/layout/header.css">
+    
+    <link rel="stylesheet" href="html/assets/css/layout/footer.css">
+    <link rel="stylesheet" href="../../assets/css/materialdesignicons.min.css">
     <title>Page d'accueil</title>
 </head>
 <body>
     <header>
         <nav>
+            <a href=""><img src="../../../images/logo_alhaiz_breizh_fullsizeh.svg" alt="Logo ALHaiZ Breizh"></a>
             <ul>
-                <li><a href=""><img src="../../logo_alhaiz_breizh_fullsizeh.svg" alt=""></a></li>
                 <li><a href="">Accueil</a></li>
                 <li><a href="">Logements</a></li>
-                <li><a href="">Connexion</a></li>
-                <li><a href="">Inscription</a></li>
+            </ul>
+            <ul>
                 <!-- Deux boutons distincts ! -->
+                <li><span class="mdi mdi-account-circle-outline"></span></li>
+                <li id="sign-in-button"><a href="">Connexion</a></li>
+                <li id="sign-up-button"><a href="">Inscription</a></li>
             </ul>
         </nav>
+        <div></div>
     </header>
     <main>
