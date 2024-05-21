52a5181424938af3b5920234633f852a0525c518
matteogaillard <matteo.gaillard@protonmail.com>
2024-05-21 09:19:26 +0200

    initialised backoffice login & new accomodation views

--- html/index.php
+++ html/index.php
@@ -11,11 +11,10 @@ $router->add("/", "views/home.php");
 // account profile & public proqfile view
 $router->add("/profil", "views/account/profile.php");
 
+// backoffices views
+$router->add("/backoffice/connexion", "views/backoffice/login.php");
+$router->add("/backoffice/logements/nouveau", "views/backoffice/accomodations/new.php");
+
 $router->set404View("views/errors/404.php");
 
 $router->start();
-
-// require_once("./models/AccommodationModel.php");
-// foreach(AccommodationModel::find(0, 10) as $accomodation) {
-//     var_dump($accomodation->get("id_logement"));
-// }
