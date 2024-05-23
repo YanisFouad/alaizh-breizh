2c77c20d08bc841fef132f208230a0b2bed4d5a0
Zanananane <jeannelebau@gmail.com>
2024-05-23 22:13:59 +0200

    Fix sur tout le html et css de corentin + page loc

--- html/views/backoffice/booking/listeReservationsProprietaire.php
+++ html/views/backoffice/booking/listeReservationsProprietaire.php
@@ -1,18 +1,15 @@
 <?php
    
     require_once(__DIR__."/../../../models/BookingModel.php");
+    include_once(__DIR__."/../layout/header.php");
 
-    // ***********************
-    // Partie session de l'utilisateur
-    // ***********************
+    if(!UserSession::isConnected()){
+        require_once("views/backoffice/authentication/login.php");
+        exit;
+    }
 
-    // if(!UserSession::isConnected()){
-    //     header("Location: /");
-    // }
-
-    // $profile = UserSession::get();
-    // $id_proprietaire = $profile->get("id_compte");
-    $id_proprietaire = 'MarLe';
+    $profile = UserSession::get();
+    $id_proprietaire = $profile->get("id_compte");
 
     function trie_date($date1, $date2){
         if ($date1 == $date2) return 0;
@@ -71,160 +68,160 @@
     <link rel="stylesheet" href="../../../assets/css/materialdesignicons.min.css">
     <title>Liste réservation</title>
 </head>
-<body id="liste-reservation-proprietaire-body">
-    
-    <div id="liste-reservation-proprietaire-entete">
-        <h1>Mes réservations</h1>
+    <body>
+        <main id="liste-reservation-proprietaire-main">
+            <div id="liste-reservation-proprietaire-entete">
+                <h1>Mes réservations</h1>
 
-        <!--Rechercher une réservation-->
-        <div class="liste-reservation-proprietaire-recherche">
-            
-            <!-- export des réservations -->
-            <form action="/controllers/csvExport.php" method="POST">
-                <!-- bouton d'export -->
-                <button id="export-reservation" class="primary backoffice export-reservation" type="submit">
-                    Exporter mes réservations
-                    <!-- <span class="mdi mdi-export-variant"></span> -->
-                </button> 
-                <!-- non de fichier pour l'export -->
-                <input type="hidden" name="name" value="export_reservations.csv" />
-                <!-- donnnée pour fichier -->
-                <input type="hidden" name="array" value="<?php echo htmlentities(serialize(array_map(function  ($res){
-                return $res->getData();
-                } ,$tab_toute_reservation_periode))); ?>" />
-            </form>
-
-            <div>
-                <!-- Textarea Rechercher -->
-                <textarea id="inputRechercher" name="inputRechercher" autocomplete="on" rows="1" disabled >Rechercher...</textarea>
-                <!-- Textarea date -->
-                <textarea id="inputDateReservations" name="inputDateReservations" autocomplete="on" rows="1" disabled >Dates de réservation...</textarea>
-            </div>
-            <button class="primary backoffice" disabled>
-                <span class="mdi mdi-magnify"></span>
-            </button>
-        </div>
-    </div>
-
-    <!-- Onglets affichages des réservations -->
-    <nav id="liste-reservation-proprietaire-onglet">
-        <a class="<?php echo $tab === "a_venir" ? "active" : "" ;?>" href="?tab=a_venir">A venir (<?php echo BookingModel::countByPeriod("a_venir", $id_proprietaire)?>)</a>    
-        <a class="<?php echo $tab === "en_cours" ? "active" : "" ;?>" href="?tab=en_cours">En cours (<?php echo BookingModel::countByPeriod("en_cours", $id_proprietaire)?>)</a>
-        <a class="<?php echo $tab === "passe" ? "active" : "" ;?>" href="?tab=passe">Passée (<?php echo BookingModel::countByPeriod("passe", $id_proprietaire)?>)</a>
-    </nav>
-    <hr>
-
-    <div id="liste-reservation-proprietaire-float-left">
-        <!-- Bouton filtre -->
-        <button class="primary backoffice liste-reservation-proprietaire-flex-row" disabled >
-            <span class="mdi mdi-filter-variant"></span>
-            Filtre
-        </button>
-
-        <!-- Bouton trie -->
-        <!-- trie pas encore fonctionnel -->
-        <!-- ?trie=<?php echo $trie === "croissant" ? "decroissant" : "croissant" ?> -->
-        <a href=""><button class="liste-reservation-proprietaire-flex-row liste-reservation-proprietaire-bouton-filtre" disabled > 
-            <span class="mdi mdi-sort-ascending"></span>
-            trier par date    
-        </button></a>
-    </div>
-
-    <!-- Traitement selon l'onglets réservations -->
-    <?php
-
-    // //Sélection du tableau à utilisé 
-    // if ($tab === "a_venir"){
-    //     $tab_reservation_filtrer_trier = $tab_reservation_a_venir;
-    // }elseif ($tab === "passe" ) {
-    //     $tab_reservation_filtrer_trier = $tab_reservation_passe;
-    // } elseif ($tab === "en_cours") {
-    //     $tab_reservation_filtrer_trier = $tab_reservation_en_cours;
-    // }
-
-    ?>
-    <!-- Liste réservation -->
-    <section id="liste-reservation-proprietaire">
-        <!-- ************************** -->
-        <!-- Traitement des réservation -->
-        <!-- ************************** -->
-        <?php
-        foreach($tab_reservation as $reservation){
-            ?>
-            <a class="non-souligne" href="chemin_page_de_yanis?id=<?php echo $id;?>">
-                <article class="liste-reservation-proprietaire-logement">
-                    <!-- Photo maison + nom maison -->
-                    <div>
-                        <div id='img-container'>
-                            <img src="<?php echo $reservation->get("photo_logement"); ?>" alt="Logement">
-                        </div>
-                        <h4><?php echo $reservation->get("titre_logement"); ?></h4>
-                    </div>
+                <!--Rechercher une réservation-->
+                <div class="liste-reservation-proprietaire-recherche">
                     
+                    <!-- export des réservations -->
+                    <form action="/controllers/csvExport.php" method="POST">
+                        <!-- bouton d'export -->
+                        <button id="export-reservation" class="primary backoffice export-reservation" type="submit">
+                            Exporter mes réservations
+                            <!-- <span class="mdi mdi-export-variant"></span> -->
+                        </button> 
+                        <!-- non de fichier pour l'export -->
+                        <input type="hidden" name="name" value="export_reservations.csv" />
+                        <!-- donnnée pour fichier -->
+                        <input type="hidden" name="array" value="<?php echo htmlentities(serialize(array_map(function  ($res){
+                        return $res->getData();
+                        } ,$tab_toute_reservation_periode))); ?>" />
+                    </form>
 
-                    <!-- Description maison -->
-                    <div class="liste-reservation-proprietaire-logement-detail">
-                        <div>
-                            <h5>Date de réservation</h5>
-                            <h4><?php echo $reservation->get("date_reservation"); ?></h4>
-                        </div>
-                        <div>
-                            <h5>Nombre de nuit</h5>
-                            <h4><?php echo $reservation->get("nb_nuit"); ?></h4>
-                        </div>
-                        <div>
-                            <h5>Prix total</h5>
-                            <h4><?php echo $reservation->get("prix_total"); ?>€</h4>
-                        </div>
-                        <button class="primary backoffice liste-reservation-proprietaire-flex-row" disabled >
-                            <span class="mdi mdi-eye-outline"></span>
-                            Facture
-                        </button>
+                    <div>
+                        <!-- Textarea Rechercher -->
+                        <textarea id="inputRechercher" name="inputRechercher" autocomplete="on" rows="1" disabled >Rechercher...</textarea>
+                        <!-- Textarea date -->
+                        <textarea id="inputDateReservations" name="inputDateReservations" autocomplete="on" rows="1" disabled >Dates de réservation...</textarea>
                     </div>
-                </article>
-            </a>
-        <?php } ?>
-    </section>
+                    <button class="primary backoffice" disabled>
+                        <span class="mdi mdi-magnify"></span>
+                    </button>
+                </div>
+            </div>
 
-    <!-- Changement de page de réservation -->
-    <form method="GET" action="#" id="liste-reservation-proprietaire-pagination">
-    
-        <!-- Bouton pagination précédent -->
-        <button name="page" value="<?php echo $page-1; ?>" class="<?php echo $page > 1 ? "button-chevron-cliquable" : "button-chevron-non-cliquable" ?>" type="submit">
-            <span class="mdi mdi-chevron-left"></span>
-        </button>
+            <!-- Onglets affichages des réservations -->
+            <nav id="liste-reservation-proprietaire-onglet">
+                <a class="<?php echo $tab === "a_venir" ? "active" : "" ;?>" href="?tab=a_venir">A venir (<?php echo BookingModel::countByPeriod("a_venir", $id_proprietaire)?>)</a>    
+                <a class="<?php echo $tab === "en_cours" ? "active" : "" ;?>" href="?tab=en_cours">En cours (<?php echo BookingModel::countByPeriod("en_cours", $id_proprietaire)?>)</a>
+                <a class="<?php echo $tab === "passe" ? "active" : "" ;?>" href="?tab=passe">Passée (<?php echo BookingModel::countByPeriod("passe", $id_proprietaire)?>)</a>
+            </nav>
+            <hr>
 
-        <!-- Bouton pagination page précédente -->
-        <?php
-            if($page-1>0){?>
-                <button name="page" class="button-cliquable" value="<?php echo $page-1 ?>" type="submit">
-                    <?php echo $page-1; ?>
+            <div id="liste-reservation-proprietaire-float-left">
+                <!-- Bouton filtre -->
+                <button class="primary backoffice liste-reservation-proprietaire-flex-row" disabled >
+                    <span class="mdi mdi-filter-variant"></span>
+                    Filtre
                 </button>
-        <?php }?>
 
-        <!-- Bouton séléctionné -->
-        <button id="button-clique">
-            <?php echo $page; ?>
-        </button>
-        
-        <!-- Bouton pagination page suivante -->
-        <?php
-            if($page+1<=$nb_page){?>
-                <button name="page" class="button-cliquable" value="<?php echo $page+1 ?>" type="submit">
-                    <?php echo $page+1; ?>
+                <!-- Bouton trie -->
+                <!-- trie pas encore fonctionnel -->
+                <!-- ?trie=<?php echo $trie === "croissant" ? "decroissant" : "croissant" ?> -->
+                <a href=""><button class="liste-reservation-proprietaire-flex-row liste-reservation-proprietaire-bouton-filtre" disabled > 
+                    <span class="mdi mdi-sort-ascending"></span>
+                    trier par date    
+                </button></a>
+            </div>
+
+            <!-- Traitement selon l'onglets réservations -->
+            <?php
+
+            // //Sélection du tableau à utilisé 
+            // if ($tab === "a_venir"){
+            //     $tab_reservation_filtrer_trier = $tab_reservation_a_venir;
+            // }elseif ($tab === "passe" ) {
+            //     $tab_reservation_filtrer_trier = $tab_reservation_passe;
+            // } elseif ($tab === "en_cours") {
+            //     $tab_reservation_filtrer_trier = $tab_reservation_en_cours;
+            // }
+
+            ?>
+            <!-- Liste réservation -->
+            <section id="liste-reservation-proprietaire">
+                <!-- ************************** -->
+                <!-- Traitement des réservation -->
+                <!-- ************************** -->
+                <?php
+                foreach($tab_reservation as $reservation){
+                    ?>
+                    <a class="non-souligne" href="chemin_page_de_yanis?id=<?php echo $id;?>">
+                        <article class="liste-reservation-proprietaire-logement">
+                            <!-- Photo maison + nom maison -->
+                            <div>
+                                <div id='img-container'>
+                                    <img src="<?php echo $reservation->get("photo_logement"); ?>" alt="Logement">
+                                </div>
+                                <h4><?php echo $reservation->get("titre_logement"); ?></h4>
+                            </div>
+                            
+
+                            <!-- Description maison -->
+                            <div class="liste-reservation-proprietaire-logement-detail">
+                                <div>
+                                    <h5>Date de réservation</h5>
+                                    <h4><?php echo $reservation->get("date_reservation"); ?></h4>
+                                </div>
+                                <div>
+                                    <h5>Nombre de nuit</h5>
+                                    <h4><?php echo $reservation->get("nb_nuit"); ?></h4>
+                                </div>
+                                <div>
+                                    <h5>Prix total</h5>
+                                    <h4><?php echo $reservation->get("prix_total"); ?>€</h4>
+                                </div>
+                                <button class="primary backoffice liste-reservation-proprietaire-flex-row" disabled >
+                                    <span class="mdi mdi-eye-outline"></span>
+                                    Facture
+                                </button>
+                            </div>
+                        </article>
+                    </a>
+                <?php } ?>
+            </section>
+
+            <!-- Changement de page de réservation -->
+            <form method="GET" action="#" id="liste-reservation-proprietaire-pagination">
+            
+                <!-- Bouton pagination précédent -->
+                <button name="page" value="<?php echo $page-1; ?>" class="<?php echo $page > 1 ? "button-chevron-cliquable" : "button-chevron-non-cliquable" ?>" type="submit">
+                    <span class="mdi mdi-chevron-left"></span>
                 </button>
-        <?php } 
-       ?>
 
-        <!-- Bouton pagination page + 1 -->
-        <button name="page" value="<?php echo $page+1; ?>" class="<?php echo $page+1 <= $nb_page ? "button-chevron-cliquable" : "button-chevron-non-cliquable";?>" type="submit">
-            <span class="mdi mdi-chevron-right"></span>
-        </button>
+                <!-- Bouton pagination page précédente -->
+                <?php
+                    if($page-1>0){?>
+                        <button name="page" class="button-cliquable" value="<?php echo $page-1 ?>" type="submit">
+                            <?php echo $page-1; ?>
+                        </button>
+                <?php }?>
 
-        <!-- champs caché contenant l'onglet en cours -->
-        <input type="hidden" id="tab-form" name="tab-form" value="<?php echo $tab;?>" />
+                <!-- Bouton séléctionné -->
+                <button id="button-clique">
+                    <?php echo $page; ?>
+                </button>
+                
+                <!-- Bouton pagination page suivante -->
+                <?php
+                    if($page+1<=$nb_page){?>
+                        <button name="page" class="button-cliquable" value="<?php echo $page+1 ?>" type="submit">
+                            <?php echo $page+1; ?>
+                        </button>
+                <?php } 
+            ?>
 
-    </form>
+                <!-- Bouton pagination page + 1 -->
+                <button name="page" value="<?php echo $page+1; ?>" class="<?php echo $page+1 <= $nb_page ? "button-chevron-cliquable" : "button-chevron-non-cliquable";?>" type="submit">
+                    <span class="mdi mdi-chevron-right"></span>
+                </button>
 
-</body>
+                <!-- champs caché contenant l'onglet en cours -->
+                <input type="hidden" id="tab-form" name="tab-form" value="<?php echo $tab;?>" />
+
+            </form>
+        </main>
+    </body>
 </html>
