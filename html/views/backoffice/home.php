82b0cd03cf384df29efe22dd8132e6bd670e8b17
Yanis Fouad <fouadyanis35@gmail.com>
2024-05-23 13:42:50 +0200

    Last fix

--- html/views/backoffice/home.php
+++ html/views/backoffice/home.php
@@ -5,15 +5,6 @@
     $controller = new HomeBackofficeController();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/main.css">
-    <link rel="stylesheet" href="/assets/css/materialdesignicons.min.css">
-    <title>Accueil</title>
-</head>
 <body>
     <?php require ("views/backoffice/layout/header.php"); ?>
     <main id="page-home-backoffice">
