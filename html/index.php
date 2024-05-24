88e4a5a5f0d8c61b486815afd50771ee80348c6b
matteogaillard <matteo.gaillard@protonmail.com>
2024-05-24 02:55:34 +0200

    change

--- html/index.php
+++ html/index.php
@@ -13,7 +13,6 @@ $router->add("/logements", "views/housing-list.php");
 // bookings
 $router->add("/finaliser-ma-reservation", "views/bookings/finalizeBooking.php");
 $router->add("/reservation", "views/bookings/booking.php");
-$router->add("/backoffice/reservation", "views/backoffice/bookings/booking.php");
 
 // account profile & public profile view
 $router->add("/profil", "views/bookings/reservation.php");
@@ -21,11 +20,8 @@ $router->add("/profil", "views/bookings/reservation.php");
 // backoffices views
 $router->add("/backoffice", "views/backoffice/home.php");
 $router->add("/backoffice/nouveau-logement", "views/backoffice/accomodations/newAccommodation.php");
+$router->add("/backoffice/reservation", "views/backoffice/bookings/booking.php");
 
 $router->set404View("views/errors/404.php");
 
 $router->start();
-
-require_once("services/session/PurchaseSession.php");
-require_once("models/AccommodationModel.php");
-PurchaseSession::set(10);
