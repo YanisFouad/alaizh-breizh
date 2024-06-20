85797bea9f59a658f8e4535b1598b3e7ef6ad6f5
matteogaillard <matteo.gaillard@protonmail.com>
2024-06-20 15:07:13 +0200

    updated profile section

---
+++ html/controllers/account/profileController.php
@@ -0,0 +1,13 @@
+<?php
+
+function obsfuceRIB($rib) {
+    $obsfuceLength = 4;
+    $length = strlen($rib);
+    $rib2keep = substr($rib, 0, $obsfuceLength);
+    return wordwrap(
+        $rib2keep . join("", array_fill(0, $length-$obsfuceLength-1,"*")), 
+        4, 
+        " ", 
+        true
+    );
+}
