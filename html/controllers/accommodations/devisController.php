07d7370f5415854425b26d6bf758e8a3eb6553d7
PierreHuger <pierre-huger@orange.fr>
2024-06-26 17:32:42 +0200

    Ajout reservation en base OK

--- html/controllers/accommodations/devisController.php
+++ html/controllers/accommodations/devisController.php
@@ -7,21 +7,28 @@ require_once(__DIR__."/../../services/session/PurchaseSession.php");
 
 if(isset($_POST)) {
     $idCompte = UserSession::get()->get("id_compte");
-    PurchaseSession::set([
+    $purcharseSession = new PurchaseSession();
+    $purcharseSession->set([
         "accommodationId" => $_POST["id_logement"],
-        "nb_voyageurs" => $_POST["nb_voyageurs"],
-        "total_ati" => $_POST["prix_total"]
+        "nb_voyageurs" => $_POST["nb_voyageur"],
+        "total_ati" => $_POST["prix_total"],
+        "date_arrivee" => $_POST["date_arriveeNF"],
+        "date_depart" => $_POST["date_departNF"]
     ]);
 
-    //BookingModel::set(id_compte, $idCompte);
-    //BookingModel::set("est_payee", false);
+    $ajoutReservation = new BookingModel();
+    $ajoutReservation->set("id_locataire", $idCompte);
     foreach($_POST as $key => $value){
-        echo "key : ".$key;
-        echo "value : " .$value;
-        //BookingModel::set($key, $value);
+        if($key != "date_arriveeNF" && $key != "date_departNF"){
+            // echo $key;
+            // echo $value;
+            $ajoutReservation->set($key, $value);
+        }
     }
-
-    //BookingModel::save();
+    $ajoutReservation->set("date_reservation", date("Y-m-d"));
+    $ajoutReservation->set("est_payee", 0);
+    var_dump($ajoutReservation);
+    $ajoutReservation->save();
     header("Content-type: application/json");
     return json_encode(["success" => true]);
 }
