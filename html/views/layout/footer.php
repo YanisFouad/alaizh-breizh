c8c0460907a69e8d39c204d505e3880b1bd879aa
Justine <justine.verger@etudiant.univ-rennes1.fr>
2024-06-27 08:25:57 +0200

    changements footer liens droits

--- html/views/layout/footer.php
+++ html/views/layout/footer.php
@@ -2,8 +2,8 @@
     <footer id="foot-global">
         <div>&copy; Copyright - ALHaiZ Breizh - 2024</div>
         <nav>
-            <div><a href="/documents/test.txt" target="_blank">Conditions générales d'utilisation <span class="mdi mdi-open-in-new"></span></a></div>
-            <div><a href="/documents/test.txt" target="_blank">Conditions générales de vente <span class="mdi mdi-open-in-new"></span></a></div>
+            <div><a href="/documents/cgu-cgv.pdf" target="_blank">Conditions générales d'utilisation et de vente <span class="mdi mdi-open-in-new"></span></a></div>
+            <div><a href="/documents/mentions-legales.pdf" target="_blank">Mentions légales <span class="mdi mdi-open-in-new"></span></a></div>
         </nav>
     </footer>
 
